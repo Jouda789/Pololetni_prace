85f220a3b8480c9bab360da52396aba689cfaef1
Judita Kindlová <j606940@gmail.com>
2014-12-31 23:09:59 +0100

    Update elipsa.java
    finální verze

--- src/cz/alej/prog/kindlova/Elipsa.java
+++ src/cz/alej/prog/kindlova/Elipsa.java
@@ -2,34 +2,29 @@ package cz.alej.prog.kindlova;
 
 public class Elipsa {
 
-	// a = hlavni poloosa, b = vedlejsi poloosa
-
 	private static boolean vElipse(double x, double y, int a, int b) {
-		return 1 >= (x*x)/(a*a) + (y*y)/(b*b) ;
+		return 1 >= (x*x)/(a*a) + (y*y)/(b*b) ; // a = hlavni poloosa, b = vedlejsi poloosa
 	}
 
 	public static void kresli(int a, int b) {
 		for (int y = -b; y <= b; y++) {
 			for (int x = -a; x <= a; x++) {
-				// char znak = vElipse(x, y, a, b)
 					if (vElipse(x, y, a, b)){System.out.printf("X");}
 					else {System.out.printf(" ");}
-				// System.out.printf("%c", znak);
 			}
 			System.out.println();
 		}
 	}
+	
 	public static void main(String[] args) {
-		
 		
 		int a =	Integer.parseInt(args[0]);
 		int b =	Integer.parseInt(args[1]);
 		
-		System.out.printf("\n");
+		System.out.println();
+		
 		kresli(a, b);
-		System.out.printf("\n");
 		
-		
+		System.out.println();
 	}
-
 }
